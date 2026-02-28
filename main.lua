57da24dcc6154e99be8cb9659a0c3446674b36a6
chicorbeep <chicory@fourrie.net>
2026-02-28 16:42:51 -0700

    .

--- main.lua
+++ main.lua
@@ -6,17 +6,21 @@ PLAYER = {
     x = 0,
     y = 0,
 	speed = 6,
+	hp = 100
 }
 
 ENEMY = {
 	x = 200,
 	y = 200,
 	speed = 2,
+	hp = 20
 }
 
+gameState = "overworld"  -- overworld, battle, or title?
 
-
-background = love.grap
+function love.conf(t)
+	t.console = true
+end
 
 function love.load()
 	playerSprite = love.graphics.newImage("assets/placeholders/player.png")
@@ -32,6 +36,47 @@ end
 function love.update(dt)
 	dtotal = dtotal + dt
 
+	if gameState == "overworld" then
+		PlayerUpdate()
+		EnemyUpdate()
+	elseif gameState == "battle" then
+		print("This is where the battle would go... IF WE HAD ANY!!")
+		ENEMY.x = 400
+		ENEMY.y = 300
+		love.timer.sleep(2)
+		gameState = "overworld"
+	else
+		print("wha")
+		love.timer.sleep(2)
+		gameState = "overworld"
+	end
+end
+
+
+function sign(x)
+	if x ~= 0 then
+		return x / math.abs(x)
+	else
+		return 0
+	end
+end
+
+
+function EnemyUpdate()
+	ENEMY.x = ENEMY.x + (ENEMY.speed)*sign(PLAYER.x-ENEMY.x)
+	ENEMY.y = ENEMY.y + (ENEMY.speed)*sign(PLAYER.y-ENEMY.y)
+
+	if (ENEMY.x == PLAYER.x and ENEMY.y == PLAYER.y) then
+		TriggerFight()
+	end
+end
+
+function TriggerFight()
+	-- placeholdler :3
+	gameState = "battle"
+end
+
+function PlayerUpdate()
 	-- movement !!!
 	if love.keyboard.isDown("down") then
 		PLAYER.y = PLAYER.y + PLAYER.speed
@@ -47,27 +92,5 @@ function love.update(dt)
 
 	if love.keyboard.isDown("right") then
 		PLAYER.x = PLAYER.x + PLAYER.speed
-	end
-
-
-end
-
-function sign(x)
-	if x ~= 0 then
-		return x / math.abs(x)
-	else
-		return 0
-	end
-end
-
-
-function EnemyUpdate()
-	enemyCollision =
-
-	ENEMY.x = ENEMY.x + (ENEMY.speed)*sign(PLAYER.x-ENEMY.x)
-	ENEMY.y = ENEMY.y + (ENEMY.speed)*sign(PLAYER.y-ENEMY.y)
-
-	if (ENEMY.x == PLAYER.x and ENEMY.y == PLAYER.y) then
-
 	end
 end
