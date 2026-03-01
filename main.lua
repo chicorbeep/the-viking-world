e3d3b9420c31cfa8c938dbad5a784e15e5804e3c
chicorbeep <chicory@fourrie.net>
2026-03-01 10:18:52 -0700

    jksdfjkD

--- main.lua
+++ main.lua
@@ -3,10 +3,7 @@
 	mi wile ala sin kepeken e toki ni!
 	toki ni li ike MUTE tawa mi.
 ]]
-mainFont = love.graphics.newFont("assets/CozetteVector.ttf", 26)
-dtotal = 0
-
-
+MAIN_FONT = love.graphics.newFont("assets/CozetteVector.ttf", 26)
 function resetEverything()
 	PLAYER = {
 	    x = 320,
@@ -23,7 +20,9 @@ function resetEverything()
 		rotation = 0
 	}
 
-	GAME_STATE = "overworld"
+    GAME_STATE = "overworld"
+    DTOTAL = 0
+
 end
 resetEverything()
 
@@ -31,7 +30,7 @@ BOARD_SCALE = 2
 BATTLE_MESSAGE = ""
 
 function love.load()
-	love.graphics.setFont(mainFont)
+	love.graphics.setFont(MAIN_FONT)
 	playerSprite = love.graphics.newImage("assets/asset-pack/Ships/ship (1).png")
 	enemySprite = love.graphics.newImage("assets/asset-pack/Ships/Pirate.png")
 	mapSprite = love.graphics.newImage("assets/map.png")
@@ -39,40 +38,40 @@ end
 
 function love.draw()
     if GAME_STATE == "overworld" then
-    	love.graphics.draw(mapSprite, -80, 0)
-		love.graphics.setBackgroundColor(0.1, 0.2, 0.7)
-		love.graphics.print(PLAYER.x.." "..PLAYER.y..";"..ENEMY.x.." "..ENEMY.y, 0, 0)
-		love.graphics.draw(playerSprite, (PLAYER.x + 33)  * BOARD_SCALE, (PLAYER.y + 56) * BOARD_SCALE, PLAYER.rotation)
-		love.graphics.draw(enemySprite, (ENEMY.x + 33) * BOARD_SCALE, (ENEMY.y + 56) * BOARD_SCALE, ENEMY.rotation)
-	elseif GAME_STATE == "battle" then
-	 	love.graphics.draw(playerSprite, 320 - 33, 480 - 59)
-		love.graphics.draw(enemySprite, 960 - 33, 480 - 59)
+        love.graphics.draw(mapSprite, -80, 26)
+        love.graphics.setBackgroundColor(0.1, 0.2, 0.7)
+        love.graphics.print("⏰ "..math.floor(DTOTAL*10)/10, 240, 0)
+        love.graphics.draw(playerSprite, (PLAYER.x + 33) * BOARD_SCALE, (PLAYER.y + 56) * BOARD_SCALE, PLAYER.rotation)
+        love.graphics.draw(enemySprite, (ENEMY.x + 33) * BOARD_SCALE, (ENEMY.y + 56) * BOARD_SCALE, ENEMY.rotation)
+
+    love.graphics.print("♥ "..PLAYER.hp.."/100", 0, 0)
+    elseif GAME_STATE == "battle" then
+        love.graphics.draw(playerSprite, 320 - 33, 480 - 59)
+        love.graphics.draw(enemySprite, 960 - 33, 480 - 59)
         love.graphics.print("BATTLE TIME!", 320, 240) -- unintentional off reference (this one has never actually played off)
-		love.graphics.print(BATTLE_MESSAGE , 320, 720)
-		love.graphics.print(PLAYER.hp.."/100", 0, 0)
-	elseif GAME_STATE == "win" then
-		love.graphics.setBackgroundColor(0, 0, 0)
-        love.graphics.print("You win!", 640, 480)
-    elseif GAME_STATE == "lose" then
-    	love.graphics.setBackgroundColor(0,0,0)
-     	love.graphics.print("Oops. You lose. Try again!")
-      	love.timer.sleep(3)
-       	resetEverything()
+        love.graphics.print(BATTLE_MESSAGE, 320, 720)
+        love.graphics.print("♥ "..PLAYER.hp .. "/100", 0, 0)
+    elseif GAME_STATE == "end" then
+        love.graphics.setBackgroundColor(0, 0, 0)
+        love.graphics.print("You survived for "..(math.floor(DTOTAL*10)/10).." seconds!", 640, 480)
     end
+
 end
 
 function love.update(dt)
-	dtotal = dtotal + dt
+	DTOTAL = DTOTAL + dt
 
 	if GAME_STATE == "overworld" then
 		PlayerUpdate()
 		EnemyUpdate()
 	elseif GAME_STATE == "battle" then
-
 		love.timer.sleep(3)
 		ENEMY.x = 400
 		ENEMY.y = 300
 		GAME_STATE = "overworld"
+	elseif GAME_STATE == "end" then
+        love.timer.sleep(3)
+        resetEverything()
 	end
 end
 
@@ -141,25 +140,26 @@ function PlayerUpdate()
 	end
 
 
-	if downHeld and not upHeld then
-		PLAYER.y = math.floor(PLAYER.y + PLAYER.speed * modifier)
-		PLAYER.rotation = 0
-	elseif upHeld and not downHeld then
-        PLAYER.y = math.floor(PLAYER.y - PLAYER.speed * modifier)
-		-- love.graphics.rotate(playerSprite)
-		PLAYER.rotation = -math.pi
-    end
+		if downHeld and not upHeld then
+			PLAYER.y = math.floor(PLAYER.y + PLAYER.speed * modifier)
+			PLAYER.rotation = 0
+		elseif upHeld and not downHeld then
+	        PLAYER.y = math.floor(PLAYER.y - PLAYER.speed * modifier)
+			-- love.graphics.rotate(playerSprite)
+			PLAYER.rotation = -math.pi
+	    end
 
-    if leftHeld and not rightHeld then
-        PLAYER.x = math.floor(PLAYER.x - PLAYER.speed * modifier)
-        PLAYER.rotation = math.pi / 2
-    elseif rightHeld and not leftHeld then
-        PLAYER.x = math.floor(PLAYER.x + PLAYER.speed * modifier)
-        PLAYER.rotation = -math.pi / 2
-    end
+	    if leftHeld and not rightHeld then
+	        PLAYER.x = math.floor(PLAYER.x - PLAYER.speed * modifier)
+	        PLAYER.rotation = math.pi / 2
+	    elseif rightHeld and not leftHeld then
+	        PLAYER.x = math.floor(PLAYER.x + PLAYER.speed * modifier)
+	        PLAYER.rotation = -math.pi / 2
+	    end
 
     if PLAYER.hp < 1 then
-    	GAME_STATE = "loss"
+        GAME_STATE = "end"
+        GameOver()
     end
 
 
